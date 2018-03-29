19fda5a678b5a6c8da29985a9a1fcf6d75d6e341
huayiqiu <284527552@qq.com>
2018-03-29 18:36:11 +0800

    <modify>first article page show

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from blog import models
 def index(request):
-    return render(request,'blog/static/index.html')
+    post_list = models.Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/static/index.html', context={'post_list': post_list})
+    # return render(request,'blog/static/index.html',)
